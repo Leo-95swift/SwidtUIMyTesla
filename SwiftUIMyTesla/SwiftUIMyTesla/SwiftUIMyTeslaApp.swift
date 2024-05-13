f4a86ea6fb57daedf96b75e666ca648fe08b9a62
xach55 <xach55@gmail.com>
2024-05-13 23:14:36 +0400

    initial commit

---
+++ SwiftUIMyTesla/SwiftUIMyTesla/SwiftUIMyTeslaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMyTeslaApp.swift
+//  SwiftUIMyTesla
+//
+//  Created by Levon Shaxbazyan on 13.05.24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMyTeslaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
